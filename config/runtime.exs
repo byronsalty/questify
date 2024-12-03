be34fc6cf14aa3779b5a080310d75312a624ecf5
Byron Salty <byronsalty@gmail.com>
2024-12-03 00:58:38 -0600

    improving ssl handling in db

--- config/runtime.exs
+++ config/runtime.exs
@@ -51,11 +51,7 @@ if config_env() == :prod do
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6,
-    ssl: true,
-    ssl_opts: [
-      server_name_indication: ~c"ep-tiny-pine-a5ub39yy.us-east-2.aws.neon.tech",
-      verify: :verify_none
-    ]
+    ssl: [cacerts: :public_key.cacerts_get()]
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
